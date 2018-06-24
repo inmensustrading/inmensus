6262eb84dcb2228a586edf0fde32bf74654e2c2a
Yang Yan <yangawesome@gmail.com>
2018-06-24 17:45:05 -0600

    .

---
+++ io-modules/gemini-im-cpp/gemini-im-cpp/main.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define _WEBSOCKETPP_CPP11_INTERNAL_
+
+#include "../../../common/websocketpp-master/websocketpp/config/asio_no_tls_client.hpp"
+#include "../../../common/websocketpp-master/websocketpp/client.hpp"
+
+#include <iostream>
