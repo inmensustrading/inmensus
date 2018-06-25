16054a8c602ae7b9cac3b2d7d1e4a7bf5e238258
Yang Yan <yangawesome@gmail.com>
2018-06-24 18:18:06 -0600

    boost 2

--- io-modules/gemini-im-cpp/gemini-im-cpp/main.h
+++ io-modules/gemini-im-cpp/gemini-im-cpp/main.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define _SCL_SECURE_NO_WARNINGS
+
 #include "../../../common/websocketpp-master/websocketpp/config/asio_no_tls_client.hpp"
 #include "../../../common/websocketpp-master/websocketpp/client.hpp"
 
