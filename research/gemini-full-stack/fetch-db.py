6a8d6cbf9f5f9dee7b3efd053599458fdfbc36e6
Yang Yan <yangawesome@gmail.com>
2018-07-16 19:32:15 -0600

    reorganization

--- research/gemini-full-stack/fetch-db.py
+++ research/gemini-full-stack/fetch-db.py
@@ -13,6 +13,7 @@ def toRelPath(origPath):
 
 import pymysql
 import json
+import numpy
 
 conn = pymysql.connect(
 	host="inmensus-trading-db-1.ce50oojfsygk.us-east-2.rds.amazonaws.com",
@@ -21,28 +22,21 @@ conn = pymysql.connect(
 	db="inmensus_trading_db_1")
 cur = conn.cursor()
 
-print("Columns:")
 cur.execute("SHOW columns FROM gemini_change;")
-for r in cur:
-    print(r)
+print("Columns:", cur.fetchall())
+
 cur.execute("SELECT count(*) FROM gemini_change;")
-print("Size: ", end="")
-for r in cur:
-    print(r)
+print("Size:", cur.fetchall())
 
-clearDB = input("Reset database? (y/n): ")
-if clearDB == "y":
-	cur.execute("DELETE FROM gemini_change;")
+cur.execute("SELECT * FROM gemini_change WHERE reason = 'connect';")
+print(cur.fetchall())
 
-	cur.execute("SELECT count(*) FROM gemini_change;")
-	print("Size: ", end="")
-	for r in cur:
-		print(r)
+#cur.execute("DELETE FROM gemini_change;")
 
 input("Enter to fetch full database...")
 
 #get all data
-cur.execute("SELECT * FROM gemini_change LIMIT 10;")
+cur.execute("SELECT * FROM gemini_change;")
 with open(toRelPath("db.json"), "w") as outfile:
     json.dump(cur.fetchall(), outfile)
 
